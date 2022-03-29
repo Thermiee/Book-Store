f5a534a27a16b146aa6c254fd7ca97ae8bdb8078
Thermiee <navyblack16@yahoo.com>
2022-03-29 22:45:27 +0100

    fix linter error

--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,6 +1,6 @@
 import { combineReducers, createStore } from 'redux';
-import books from './books/Books';
-import categories from './categories/Categories';
+import books from './books/books';
+import categories from './categories/categories';
 
 const rootReducer = combineReducers({
   books,
