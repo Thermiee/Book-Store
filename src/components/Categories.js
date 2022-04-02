5d7c90786d8af14f08640ac685f31aab87b3712f
Thermiee <navyblack16@yahoo.com>
2022-04-02 13:56:34 +0100

    Add Styling to categories

--- src/components/Categories.js
+++ src/components/Categories.js
@@ -1,3 +1,3 @@
-const Categories = () => <h1 className="title">Under construction</h1>;
+const Categories = () => <h1 className="categories-title">Under construction</h1>;
 
 export default Categories;
